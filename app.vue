da80503c4921e0f35e513773920cd2e4a8181d5a
Laurenz_Saam <laurenz.saam@umlaut.com>
2022-05-02 10:03:32 +0200

    second commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <form netlify action="/" method="POST" name="Contact">
+      <input type="hidden" name="form-name" value="Contact" />
+      <input type="text" name="name" id="" />
+      <button type="submit">Send</button>
+    </form>
+  </div>
+</template>
